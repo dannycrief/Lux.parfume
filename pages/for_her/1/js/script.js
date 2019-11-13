ba065d5c851073eb3d428c7420e59b27a29f67b7
danny <step.kozbvb@gmail.com>
2019-11-13 17:28:51 +0100

    photos and js module were apdated

--- pages/for_her/1/js/script.js
+++ pages/for_her/1/js/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-	
+function showNext(){
+	if($('#exampleModalScrollable').modal('show')){
+		$('.fade').hide();
+		$('#exampleModalScrollable').hide();
+		$('#exampleModalCenter').modal('show');
+		console.log('if show');
+	}	
 }
